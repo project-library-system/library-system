ec6bcd6649dbcfa8bd17dcf23aa6ebd55b31f0c4
luizeduardo1953 <luizeduardo1953@gmail.com>
2026-05-17 18:25:20 -0300

    Cors Configured

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new GlobalExceptionFilter());
+
+  app.enableCors({
+    origin: 'http://localhost:3000', //Rota do front que vai acessar a API
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', //Métodos HTTP que serão aceitos
+    credentials: true, //permite cookies e headers personalizados
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization', //Autorização para autenticação
+    ],
+  });
+
   await app.listen(8000, '0.0.0.0');
 }
 bootstrap();
